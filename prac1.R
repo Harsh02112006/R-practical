f5d08f19943ae681c245c9e9e9ea5c04c4af9893
Harsh02112006 <harshbelamkar6@gmail.com>
2025-10-06 17:17:02 +0530

    Add files via upload

---
+++ prac1.R
@@ -0,0 +1,31 @@
+m = c(1,5,3,7)
+length(m)
+t=sort(m)
+t
+rev(m)
+sqrt(m)
+mean(m)
+median(m)
+exp(m)
+log(m)
+x=8
+y=12
+z=16
+a=x^2+3*y+z
+a
+
+b=17
+d=2*b+3
+d
+r=c(5,-6,3,2,-1,-7)
+abs(r)
+p=c(5,1,7,3,9)
+rank(p)
+range(p)
+u = c(6,7,6,8,7,8,9,10,10)
+v=table(u)
+transform(v)
+e=c(1,1,5,5,3,3,9,9,7,7,3,5,7,1)
+f=1:7
+g=data.frame(e,f)
+print(g)
